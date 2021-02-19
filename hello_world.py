7058bfa341e6a00737514047cdc8ffdde13d3b53
kaylastuhlmann <kstuhlma@bowdoin.edu>
2021-02-18 23:30:31 -0500

    init hello world

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+"""
+Our first Python test program.
+Team members: Lisa Mekonnen, Kayla Stuhlmann, and Kathleem Kemp
+"""
+
+def main():
+    print("Hello, world!")
+
+if __name__ == "__main__":
+    main()
